8823082b47a0ddd48226985b412adaa75fb50052
Jacob Haslehurst <jacob@haslehurst.net>
2017-07-07 09:01:02 +0800

    hook up error chain

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,26 @@
+#[macro_use]
+extern crate error_chain;
+
 use std::io::Read;
 use std::fs::File;
 use std::path::Path;
+
+mod errors;
+
+use errors::*;
 
 pub struct FitFile {
     bytes: Vec<u8>,
 }
 impl FitFile {
-    pub fn open<P: AsRef<Path>>(path: P) -> FitFile {
-        FitFile {
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<FitFile> {
+        Ok(FitFile {
             bytes: {
                 let mut buf = vec![];
-                let mut f = File::open(path).unwrap();
-                f.read_to_end(&mut buf).unwrap();
+                let mut f = File::open(path)?;
+                f.read_to_end(&mut buf)?;
                 buf
             },
-        }
+        })
     }
 }
