26db99333e13e46a1603be8479e0a6a91b2f211d
Jacob Haslehurst <jacob@haslehurst.net>
2017-07-07 08:53:04 +0800

    add fitfile struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+use std::io::Read;
+use std::fs::File;
+use std::path::Path;
+
+pub struct FitFile {
+    bytes: Vec<u8>,
+}
+impl FitFile {
+    pub fn open<P: AsRef<Path>>(path: P) -> FitFile {
+        FitFile {
+            bytes: {
+                let mut buf = vec![];
+                let mut f = File::open(path).unwrap();
+                f.read_to_end(&mut buf).unwrap();
+                buf
+            },
+        }
     }
 }
