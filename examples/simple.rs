f2229271b42749b26b3ca628fc18f1ba2f0030e2
Jacob Haslehurst <jacob@haslehurst.net>
2017-07-07 11:21:19 +0800

    fix header parsing, add validate_data method

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,7 +2,8 @@ extern crate fit;
 
 fn main() {
     let fitfile = fit::FitFile::open("examples/data/activity_commute.fit").unwrap();
-    let header = fitfile.parse_header();
+    let header = fitfile.get_header().unwrap();
     println!("{:?}", header);
+    fitfile.validate_data().unwrap();
     println!("opened fitfile successfully");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ extern crate error_chain;
 #[macro_use]
 extern crate nom;
 
-use nom::{le_u8, le_u32};
+use nom::{le_u8, le_u16, le_u32};
 
 use std::io::Read;
 use std::fs::File;
@@ -27,8 +27,18 @@ impl FitFile {
             },
         })
     }
-    pub fn parse_header(&self) -> Result<FitFileHeader> {
-        Ok(fitfile_header(&self.bytes).unwrap().1)
+    pub fn get_header(&self) -> Result<FitFileHeader> {
+        fitfile_header(&self.bytes)
+            .to_result()
+            .map_err(|e| e.description().into())
+    }
+    pub fn validate_data(&self) -> Result<()> {
+        let header = self.get_header()?;
+        let data_size = self.bytes.len() - header.header_size as usize - 2;
+        match data_size == header.data_size as usize {
+            true => Ok(()),
+            false => Err("Data looks to be corrupted".into()),
+        }
     }
 }
 
@@ -36,22 +46,25 @@ impl FitFile {
 pub struct FitFileHeader {
     header_size: u8,
     protocol_version: u8,
-    profile_version: u8,
+    profile_version: u16,
     data_size: u32,
+    crc: u16,
 }
 
 named!(fitfile_header <FitFileHeader>, do_parse!(
     header_size: le_u8      >>
     protocol_version: le_u8 >>
-    profile_version: le_u8  >>
+    profile_version: le_u16 >>
     data_size: le_u32       >>
-    // tag!(".FIT")            >>
+    tag!(".FIT")            >>
+    crc: le_u16             >>
     (
         FitFileHeader {
             header_size: header_size,
             protocol_version: protocol_version,
             profile_version: profile_version,
             data_size: data_size,
+            crc: crc,
         }
     )
 ));
