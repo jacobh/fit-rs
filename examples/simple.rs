1e6db0cbbc6797dfecc60ce6114be13c4cfe5aa5
Jacob Haslehurst <jacob@haslehurst.net>
2017-07-07 10:27:23 +0800

    Parse header of fitfile

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,5 +2,7 @@ extern crate fit;
 
 fn main() {
     let fitfile = fit::FitFile::open("examples/data/activity_commute.fit").unwrap();
+    let header = fitfile.parse_header();
+    println!("{:?}", header);
     println!("opened fitfile successfully");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #[macro_use]
 extern crate error_chain;
+#[macro_use]
+extern crate nom;
+
+use nom::{le_u8, le_u32};
 
 use std::io::Read;
 use std::fs::File;
@@ -23,4 +27,31 @@ impl FitFile {
             },
         })
     }
+    pub fn parse_header(&self) -> Result<FitFileHeader> {
+        Ok(fitfile_header(&self.bytes).unwrap().1)
+    }
 }
+
+#[derive(Debug)]
+pub struct FitFileHeader {
+    header_size: u8,
+    protocol_version: u8,
+    profile_version: u8,
+    data_size: u32,
+}
+
+named!(fitfile_header <FitFileHeader>, do_parse!(
+    header_size: le_u8      >>
+    protocol_version: le_u8 >>
+    profile_version: le_u8  >>
+    data_size: le_u32       >>
+    // tag!(".FIT")            >>
+    (
+        FitFileHeader {
+            header_size: header_size,
+            protocol_version: protocol_version,
+            profile_version: profile_version,
+            data_size: data_size,
+        }
+    )
+));
