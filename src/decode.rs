829a7013e23c9dc7e664ff387604cf06bf46a92e
Jacob Haslehurst <jacob@haslehurst.net>
2017-07-10 22:44:23 +0800

    implement handle_message_available

--- src/decode.rs
+++ src/decode.rs
@@ -14,6 +14,36 @@ pub struct Message {
     num: u16,
 }
 
+fn handle_message_available(state: &mut ffi::FIT_CONVERT_STATE) {
+    let state_raw_ptr = state as *mut ffi::FIT_CONVERT_STATE;
+    let message_data = unsafe{*ffi::FitConvert_GetMessageData(state_raw_ptr)};
+    let message_num: ffi::FIT_MESG = unsafe {
+        let num = ffi::FitConvert_GetMessageNumber(state_raw_ptr) as u32;
+        ::std::mem::transmute(num)
+    };
+    
+    match message_num {
+        ffi::FIT_MESG::FIT_MESG_FILE_ID => {
+            // transmute not working
+            // unsafe {
+            //     let id: ffi::FIT_FILE_ID_MESG = ::std::mem::transmute(message_data);
+            //     println!("file id: type={} number={}", id.type, id.number);
+            // }
+            println!("file id");
+        },
+        ffi::FIT_MESG::FIT_MESG_USER_PROFILE => unimplemented!(),
+        ffi::FIT_MESG::FIT_MESG_ACTIVITY => {
+            println!("activity");
+        },
+        ffi::FIT_MESG::FIT_MESG_SESSION => unimplemented!(),
+        ffi::FIT_MESG::FIT_MESG_LAP => unimplemented!(),
+        ffi::FIT_MESG::FIT_MESG_RECORD => unimplemented!(),
+        ffi::FIT_MESG::FIT_MESG_EVENT => unimplemented!(),
+        ffi::FIT_MESG::FIT_MESG_DEVICE_INFO => unimplemented!(),
+        _ => return,
+    }
+}
+
 pub fn decode<P: AsRef<Path>>(path: P) -> Vec<Message> {
     let mut reader = {
         // let mut buf = vec![];
@@ -43,17 +73,13 @@ pub fn decode<P: AsRef<Path>>(path: P) -> Vec<Message> {
                         ffi::FIT_CONVERT_RETURN::FIT_CONVERT_CONTINUE => continue,
                         ffi::FIT_CONVERT_RETURN::FIT_CONVERT_MESSAGE_AVAILABLE => {
                             println!("message available!");
-                            messages.push(Message{
-                                data: *ffi::FitConvert_GetMessageData(&mut state as *mut ffi::FIT_CONVERT_STATE),
-                                num: ffi::FitConvert_GetMessageNumber(&mut state as *mut ffi::FIT_CONVERT_STATE),
-                            });
+                            handle_message_available(&mut state);
                         },
                         ffi::FIT_CONVERT_RETURN::FIT_CONVERT_MESSAGE_NUMBER_FOUND => unimplemented!(),
                         ffi::FIT_CONVERT_RETURN::FIT_CONVERT_ERROR => panic!(),
                         ffi::FIT_CONVERT_RETURN::FIT_CONVERT_END_OF_FILE => break,
                         ffi::FIT_CONVERT_RETURN::FIT_CONVERT_PROTOCOL_VERSION_NOT_SUPPORTED => panic!("Protocol version not supported"),
                         ffi::FIT_CONVERT_RETURN::FIT_CONVERT_DATA_TYPE_NOT_SUPPORTED => panic!("Data type not supported"),
-                        // _ => unimplemented!(),
                     }
                 }
             }
